dbf294ed137c93789fa336dd4eaae1003db50751
onthekaki <onthekaki@gmail.com>
2021-10-04 16:49:31 +1100

    eslint

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,16 @@
 </template>
 
 <script>
+import HelloWorld from '~/components/HelloWorld' // 컴포넌트 연결 예시
+
 export default {
+    components: { 
+    HelloWorld //컴포넌트 연결 예시
+  },
   data(){
+     components:{
+    HelloWorld
+    }
     return{
       message: 'Hello Vue!'
     }
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <!-- <img src="~assets/logo.png" alt="샘플" />  : 파일 연결 -->
+  <h1>Hello World</h1>
+ 
+</template>
